8aeccde8423106e9bedead6d468cb19b5ffa9992
remelendez <robinsonmelendez20@gmail.com>
2021-03-02 18:42:47 +0000

    main.cpp

--- test.cpp
+++ test.cpp
@@ -1,2 +1,55 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+
+class Quaternion {
+  
+  public: double e, f, g, h;
+
+    Quaternion(double p, q, r, s)
+    {
+      e=p;
+      f=q;
+      g=r;
+      h=s;
+    }
+    bool operator == (const Quaternion p)
+    {
+     if ((p->e==this->x) && p->f==this->f) && p->g==this->g) && p->h==this->h))
+
+     return true;
+
+     else 
+
+     return false;
+    }
+    Quaternion operator+(const Quaternion p)
+    {
+      Quaternion r;
+      r.e=p.e+this->e;
+      r.f=p.f+this->f;
+      r.g=p.g+this->g;
+      r.h=p.h+this->h;
+      
+      return r;
+    }
+    Quaternion operator - (const Quaterion p)
+    {
+      Quaterion r;
+      r.e=p.e-this->e;
+      r.f=p.f-this->f;
+      r.g=p.g-this->g;
+      r.h=p.h-this->h;
+      
+      return r;
+    }
+    Quaternion operator * (double s)
+    {
+      Quaternion p;
+      p.e=s* this->e;
+      p.f=s* this->f;
+      p.g=s* this->g;
+      p.h=s* this->h;
+      
+      return p;
+    }
+};
